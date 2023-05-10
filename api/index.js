3277d862641d6bf804206a73650486bd34e89a89
Mburu <abdallwario96@gmail.com>
2023-05-10 11:30:44 +0300

    jwt

--- api/index.js
+++ api/index.js
@@ -98,19 +98,25 @@ app.post('/register', async(req,res)=>{
       const newPath = path+'.'+ext;
       fs.renameSync(path, newPath);
     
-      const {token} = req.cookies;
-      jwt.verify(token, secret, {}, async (err,info) => {
-        if (err) throw err;
-        const {title,summary,content} = req.body;
-        const postDoc = await Post.create({
-          title,
-          summary,
-          content,
-          cover:newPath,
-          author:info.id,
-        });
-        res.json(postDoc);
+
+      const {title,summary,content} = req.body;
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+        author:info.id,
       });
+      res.json(postDoc);
+
+      // const {token} = req.cookies;
+
+
+      // jwt.verify(token, secret, {}, async (err,info) => {
+      //   if (err) throw err;
+
+       
+      // });
     
     });
 
